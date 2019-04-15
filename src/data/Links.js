7d4288cb1aa61058ebede524cae60028534cf9b0
Shane4666 <sc897@nau.edu>
2019-04-14 23:04:23 -0700

    Links and scripts update.

--- src/data/Links.js
+++ src/data/Links.js
@@ -1,26 +1,40 @@
 export const listOfLinks = {
   Productive: [
     {
-      link: "https://todoist.com/",
-      label: "ToDoIst"
+      link: 'https://todoist.com/',
+      label: 'ToDoIst'
     },
     {
-      link: "https://webmail.gm.com/",
-      label: "Work Email"
+      link: 'https://webmail.gm.com/',
+      label: 'Work Email'
     },
     {
-      link: "https://linkedin.com/",
-      label: "LinkedIn"
+      link: 'https://linkedin.com/',
+      label: 'LinkedIn'
     }
   ],
   CourseWork: [
     {
-      link: "https://udemy.com",
-      label: "Udemy"
+      link: 'https://udemy.com',
+      label: 'Udemy'
     },
     {
-      link: "https://frontendmasters.com",
-      label: "FrontEndMasters"
+      link: 'https://frontendmasters.com',
+      label: 'FrontEndMasters'
+    }
+  ],
+  Browsing: [
+    {
+      link: 'https://reddit.com',
+      label: 'Reddit'
+    },
+    {
+      link: 'https://news.google.com/?hl=en-US&gl=US&ceid=US:en',
+      label: 'Google News'
+    },
+    {
+      link: 'https://twitter.com',
+      label: 'Twitter'
     }
   ]
 };
