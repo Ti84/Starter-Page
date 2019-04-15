d1eba2282e6e85abc805c3feea9a342a64ef04d7
Shane4666 <sc897@nau.edu>
2019-04-14 22:52:56 -0700

    Updates

---
+++ precache-manifest.b8034bbab1f5c7e8b511f7e78bcae68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9376fe44e558f6710a94fd237fdfa50",
+    "url": "/Starter-Page/static/media/morgana-background.f9376fe4.jpg"
+  },
+  {
+    "revision": "59b89f3f5bb34ad118da",
+    "url": "/Starter-Page/static/js/runtime~main.52ace986.js"
+  },
+  {
+    "revision": "961b6f932713955c60e3",
+    "url": "/Starter-Page/static/js/main.7d4d4d09.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Starter-Page/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "961b6f932713955c60e3",
+    "url": "/Starter-Page/static/css/main.fef6c9eb.chunk.css"
+  },
+  {
+    "revision": "fa6e88b86db12ce0ca6cfbc5d4c98aba",
+    "url": "/Starter-Page/index.html"
+  }
+];
